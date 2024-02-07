fe130d1837ca18c9fe168abc2d76df87c4c3df91
raghuprasadks <prasadraghuks@gmail.com>
2024-02-07 15:24:12 +0530

    updated

--- programs/seleniumtraining/src/test/java/seleniumtraining/SampleWebsite_testng_3.java
+++ programs/seleniumtraining/src/test/java/seleniumtraining/SampleWebsite_testng_3.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -35,4 +36,56 @@ public class SampleWebsite_testng_3 {
 	    Assert.assertEquals(actualTitle, expectedTitle);
 	    driver.close();
   }
+@Test(priority=3)
+public void register() {
+	  System.out.println("openRegistration");
+	  System.setProperty("webdriver.chrome.driver", "D:\\kaushalya\\consultancy\\kristu\\jan2024\\drivers\\chromedriver.exe");
+	  	WebDriver driver = new ChromeDriver();
+	    String baseUrl = "https://raghuprasadks.github.io/samplewebsite/register.html";					
+	    driver.get(baseUrl);
+	    
+	    WebElement name = driver.findElement(By.id("name"));
+        name.sendKeys("raghu");
+        
+        WebElement email = driver.findElement(By.name("email"));
+        email.sendKeys("prasadraghuks@gmail.com");
+        WebElement mobile = driver.findElement(By.id("mobile"));
+        mobile.sendKeys("9845547471");
+        
+        WebElement password = driver.findElement(By.id("password"));
+        password.sendKeys("test@123");
+        
+        WebElement gender = driver.findElement(By.id("female"));
+        gender.click();
+        
+        WebElement ug = driver.findElement(By.id("ug"));
+        ug.click();
+        
+        WebElement pg = driver.findElement(By.id("pg"));
+        pg.click();
+        
+        
+        WebElement dob = driver.findElement(By.id("bob"));
+        dob.sendKeys("01-01-2000");
+        
+        Select loc = new Select(driver.findElement(By.id("location")));
+        loc.selectByVisibleText("Hubli");
+        
+        driver.findElement(By.id("additonal")).sendKeys("nil");
+        
+        WebElement btn = driver.findElement(By.xpath("/html/body/div/form/input[11]"));
+        btn.click();
+        
+        WebElement pageTitle = driver.findElement(By.tagName("h1"));
+	    String actualTitle = pageTitle.getText();
+	    String expectedTitle = "Registration successful";      
+	    Assert.assertEquals(actualTitle, expectedTitle);
+	
+        
+        
+        
+	   
+
+
+}
 }
